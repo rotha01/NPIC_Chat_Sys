2032a05f0d018b474de67bf098209693f517e22f
rotha01 <rothakhon10@gmail.com>
2026-06-28 12:45:40 +0700

    session-3.1 link-router

--- vuejs-app/src/main.js
+++ vuejs-app/src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
