90488e6a03fae24bf0db1ceaf87f7e273e443259
rotha01 <rothakhon10@example.com>
2026-07-20 02:13:51 +0700

    email-verification session-3.3

--- laravel-app/app/Http/Controllers/API/AuthController.php
+++ laravel-app/app/Http/Controllers/API/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\SendVerificationEmailRequest;
 use App\Http\Requests\User\SigninRequest;
 use App\Http\Requests\User\SignupRequest;
 use App\Http\Resources\User\UserResource;
@@ -15,13 +16,13 @@ class AuthController extends Controller
 {
     function signup(SignupRequest $request)
     {
-        
-
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => $request->password,
         ]);
+
+        $user->sendEmailVerificationNotification($request->callback_url);
 
         return response([
             'message' => 'User signed up.',
@@ -31,8 +32,13 @@ class AuthController extends Controller
 
     function signin(SigninRequest $request)
     {
-       
         $user = User::where('email', $request->email)->first();
+
+        if (!$user->hasVerifiedEmail()) {
+            throw ValidationException::withMessages([
+                'email' => 'Email is not verified.',
+            ]);
+        }
 
         if (!Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
@@ -44,7 +50,7 @@ class AuthController extends Controller
 
         return response([
             'message' => 'User signed in.',
-            'user' => $user,
+            'user' => new UserResource($user),
             'token' => $token
         ], 200);
     }
@@ -57,8 +63,8 @@ class AuthController extends Controller
         $user->currentAccessToken()->delete();
 
         // option 2
-        // $currentToken = $user->currentAccessToken();
-        // $user->tokens()->where('id', $currentToken->id)->delete();
+        $currentToken = $user->currentAccessToken();
+        $user->tokens()->where('id', $currentToken->id)->delete();
 
         return response([
             'message' => 'User signed out.'
@@ -69,7 +75,41 @@ class AuthController extends Controller
     {
         return response([
             'message' => 'Token is valid.',
-            'user' => $request->user()
+            'user' => new UserResource($request->user())
+        ], 200);
+    }
+
+    function verifyEmail(Request $request)
+    {
+        $user = User::findOrFail($request->route('id'));
+
+        if ($user->hasVerifiedEmail()) {
+            throw ValidationException::withMessages([
+                'email' => 'Email is already verified.',
+            ]);
+        }
+
+        $user->markEmailAsVerified();
+
+        return response([
+            'message' => 'Email verified successfully.'
+        ], 200);
+    }
+
+    function sendVerificationEmail(SendVerificationEmailRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if ($user->hasVerifiedEmail()) {
+            throw ValidationException::withMessages([
+                'email' => 'Email is already verified.',
+            ]);
+        }
+
+        $user->sendEmailVerificationNotification($request->callback_url);
+
+        return response([
+            'message' => 'Verification email resent.'
         ], 200);
     }
 }
